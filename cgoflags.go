cff607a76d162804a0ccacd9b9713c86397ffeb2
Sebastien Binet <binet@cern.ch>
2013-12-17 11:41:16 +0100

    all: update for go-1.2

--- cgoflags.go
+++ cgoflags.go
@@ -2,6 +2,7 @@ package croot
 
 // #include "croot/croot.h"
 // #cgo CFLAGS: -Ibindings/inc
+// #cgo CXXFLAGS: -Ibindings/inc
 import "C"
 
 // EOF
--- lib.cxx
+++
@@ -1,3 +0,0 @@
-#include "bindings/src/croot_class.cxx"
-#include "bindings/src/croot.cxx"
-#include "bindings/src/croot_hist.cxx"
