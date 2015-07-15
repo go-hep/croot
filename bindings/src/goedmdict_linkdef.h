7769eed585dba0a308e27cc48cfe6a44bd33198d
Sebastien Binet <binet@cern.ch>
2015-07-15 14:40:18 +0200

    bindings: generate dict for go-edm for root5/root6

---
+++ bindings/src/goedmdict_linkdef.h
@@ -0,0 +1,6 @@
+#if defined(__ROOTCLING__)
+#pragma link C++ class golang::gostring;
+#pragma link C++ class golang::goslice<double>;
+#pragma link C++ class golang::goslice<float>;
+#pragma link C++ class golang::goslice<int>;
+#endif
