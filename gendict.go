72cae9abeafe8454b80cb92e94999a050339f0b7
Sebastien Binet <binet@cern.ch>
2015-07-15 17:36:19 +0200

    croot: consolidate gendict_xxx

---
+++ gendict.go
@@ -0,0 +1,36 @@
+package croot
+
+import "reflect"
+
+// RegisterType declares the (equivalent) C-layout of value v to ROOT so
+// values of the same type than v can be written out to ROOT files
+func RegisterType(v interface{}) {
+	rv := reflect.ValueOf(v)
+	if rv.Kind() == reflect.Ptr {
+		rv = rv.Elem()
+	}
+	t := rv.Type()
+	//fmt.Printf("registering [%s] (sz:%d)...\n",t, t.Size())
+	gendict(t)
+}
+
+func follow_ptr(v reflect.Value) reflect.Value {
+	for {
+		switch v.Kind() {
+		case reflect.Ptr:
+			if v.Elem().Kind() == reflect.Ptr {
+				v = v.Elem()
+			} else {
+				return v
+			}
+		default:
+			return v
+		}
+	}
+	return v
+}
+
+func to_cxx_name(t reflect.Type) string {
+	//return fmt.Sprintf("::golang::%s::%s", t.PkgPath(), t.Name())
+	return t.Name()
+}
--- gendict_root6.go
+++ gendict_root6.go
@@ -4,11 +4,6 @@ package croot
 
 import "reflect"
 
-func to_cxx_name(t reflect.Type) string {
-	//return fmt.Sprintf("::golang::%s::%s", t.PkgPath(), t.Name())
-	return t.Name()
-}
-
 func gendict(t reflect.Type) {
 	panic("gendict not implemented for ROOT-6")
 }
