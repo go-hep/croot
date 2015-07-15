f745b4e7421f7b43cb55e0e730253d6020dbced0
Sebastien Binet <binet@cern.ch>
2015-07-15 18:43:17 +0200

    gendict: first root6-stab

--- gendict_root6.go
+++ gendict_root6.go
@@ -2,8 +2,28 @@
 
 package croot
 
-import "reflect"
+// #include "croot/croot.h"
+// #include <string.h>
+// #include <stdlib.h>
+import "C"
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+
+	"github.com/go-hep/croot/cgentype"
+)
 
 func gendict(t reflect.Type) {
+	code := cgentype.Generate(t)
+	fmt.Printf("code=%v\n===\n", code)
+	c_code := C.CString(code)
+	defer C.free(unsafe.Pointer(c_code))
+
+	ok := c2bool(C.CRoot_Interpreter_LoadText(C.CRoot_gInterpreter, c_code))
+	if !ok {
+		panic("oops")
+	}
 	panic("gendict not implemented for ROOT-6")
 }
