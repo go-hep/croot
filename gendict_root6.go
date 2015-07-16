5cd4b0d93e62eb165d0582d4443d1d4a1c1510e7
Sebastien Binet <binet@cern.ch>
2015-07-16 17:44:00 +0200

    croot: generate dicts with CLing

--- gendict_root6.go
+++ gendict_root6.go
@@ -15,14 +15,74 @@ import (
 	"github.com/go-hep/croot/cgentype"
 )
 
+// map of already translated-to-Cling types
+var clinged_types map[reflect.Type]struct{}
+
+func init() {
+	clinged_types = make(map[reflect.Type]struct{})
+	init_cling()
+}
+
 func gendict(t reflect.Type) {
+	if _, ok := clinged_types[t]; ok {
+		return
+	}
+
+	switch t.Kind() {
+	case reflect.Bool,
+		reflect.Int,
+		reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint,
+		reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64,
+		reflect.Complex64, reflect.Complex128,
+		reflect.Uintptr:
+		// no-op
+		return
+
+	case reflect.Array:
+		gendict(t.Elem())
+		return
+
+	case reflect.Struct:
+		for i := 0; i < t.NumField(); i++ {
+			gendict(t.Field(i).Type)
+		}
+
+	case reflect.Interface:
+		panic(fmt.Errorf(
+			"croot: cannot generate dictionary for interface [%s]",
+			t.Name(),
+		))
+
+	case reflect.Chan:
+		panic(fmt.Errorf(
+			"croot: cannot generate dictionary for channel [%s]", t.Name(),
+		))
+	}
+
 	code := cgentype.Generate(t)
 	c_code := C.CString(code)
 	defer C.free(unsafe.Pointer(c_code))
 
 	ok := c2bool(C.CRoot_Interpreter_LoadText(C.CRoot_gInterpreter, c_code))
 	if !ok {
-		panic("oops")
+		panic(fmt.Errorf(
+			"croot: failed to generate dictionary for [%s]",
+			t.Name(),
+		))
 	}
-	panic("gendict not implemented for ROOT-6")
+	clinged_types[t] = struct{}{}
+}
+
+func init_cling() {
+	c_code := C.CString(`
+#include <stdint.h>
+`)
+	defer C.free(unsafe.Pointer(c_code))
+
+	ok := c2bool(C.CRoot_Interpreter_LoadText(C.CRoot_gInterpreter, c_code))
+	if !ok {
+		panic("croot: could not initialize CLing interpreter")
+	}
 }
