ad6aabee89d75db43f1e9540877bc32ab68e98c4
Sebastien Binet <binet@cern.ch>
2015-07-21 19:03:07 +0200

    class: test layout of golang::gostring and golang::goslice<T>

--- class_test.go
+++ class_test.go
@@ -7,79 +7,117 @@ import (
 )
 
 func TestClass(t *testing.T) {
-	cls := croot.GetClass("golang::gostring")
-	if cls == nil {
-		t.Fatalf("could not retrieve croot.Class for 'golang::gostring'\n")
-	}
-
-	cls.Print("")
-
-	for _, table := range []struct {
+	type MemberDescr struct {
 		Name         string
 		Offset       int
 		TypeName     string
 		FullTypeName string
 		ArrayDim     int
-	}{
-		{
-			Name:         "Len",
-			Offset:       0,
-			TypeName:     "int",
-			FullTypeName: "int",
-			ArrayDim:     0,
-		},
-		{
-			Name:         "Data",
-			Offset:       8,
-			TypeName:     "char",
-			FullTypeName: "char*",
-			ArrayDim:     0,
-		},
-	} {
-
-		dm := cls.GetDataMember(table.Name)
-		if dm == nil {
-			t.Fatalf(
-				"could not retrieve %q data member from 'golang::gostring'\n",
-				table.Name,
-			)
-		}
-
-		offset := dm.GetOffset()
-		if table.Offset != offset {
-			t.Fatalf(
-				"golang::gostring.%s: offset error. want=%d. got=%d\n",
-				table.Name, table.Offset,
-				offset,
-			)
-		}
-
-		tname := dm.GetTypeName()
-		if table.TypeName != tname {
-			t.Fatalf(
-				"golang::gostring.%s: typename error. want=%q. got=%q\n",
-				table.Name, table.TypeName,
-				tname,
-			)
-		}
-
-		ftname := dm.GetFullTypeName()
-		if table.FullTypeName != ftname {
-			t.Fatalf(
-				"golang::gostring.%s: full-typename error. want=%q. got=%q\n",
-				table.Name, table.FullTypeName,
-				ftname,
-			)
-		}
-
-		ndims := dm.GetArrayDim()
-		if table.ArrayDim != ndims {
-			t.Fatalf(
-				"golang::gostring.%s: ndims error. want=%d. got=%d\n",
-				table.Name, table.TypeName,
-				ndims,
-			)
-		}
 	}
 
+	for _, table := range []struct {
+		Name    string
+		Members []MemberDescr
+	}{
+		{
+			Name: "golang::gostring",
+			Members: []MemberDescr{
+				{
+					Name:         "Len",
+					Offset:       0,
+					TypeName:     "int",
+					FullTypeName: "int",
+					ArrayDim:     0,
+				},
+				{
+					Name:         "Data",
+					Offset:       8,
+					TypeName:     "char",
+					FullTypeName: "char*",
+					ArrayDim:     0,
+				},
+			},
+		},
+		{
+			Name: "golang::goslice<double>",
+			Members: []MemberDescr{
+				{
+					Name:         "Len",
+					Offset:       0,
+					TypeName:     "int",
+					FullTypeName: "int",
+					ArrayDim:     0,
+				},
+				{
+					Name:         "Cap",
+					Offset:       4,
+					TypeName:     "int",
+					FullTypeName: "int",
+					ArrayDim:     0,
+				},
+				{
+					Name:         "Data",
+					Offset:       8,
+					TypeName:     "double",
+					FullTypeName: "double*",
+					ArrayDim:     0,
+				},
+			},
+		},
+	} {
+		cls := croot.GetClass(table.Name)
+		if cls == nil {
+			t.Fatalf("could not retrieve croot.Class for %q\n", table.Name)
+		}
+
+		cls.Print("")
+
+		for _, m := range table.Members {
+
+			dm := cls.GetDataMember(m.Name)
+			if dm == nil {
+				t.Fatalf(
+					"could not retrieve %q data member from %q\n",
+					m.Name,
+					table.Name,
+				)
+			}
+
+			offset := dm.GetOffset()
+			if m.Offset != offset {
+				t.Fatalf(
+					"%s.%s: offset error. want=%d. got=%d\n",
+					table.Name, m.Name, m.Offset,
+					offset,
+				)
+			}
+
+			tname := dm.GetTypeName()
+			if m.TypeName != tname {
+				t.Fatalf(
+					"%s.%s: typename error. want=%q. got=%q\n",
+					table.Name, m.Name, m.TypeName,
+					tname,
+				)
+			}
+
+			ftname := dm.GetFullTypeName()
+			if m.FullTypeName != ftname {
+				t.Fatalf(
+					"%s.%s: full-typename error. want=%q. got=%q\n",
+					table.Name, m.Name, m.FullTypeName,
+					ftname,
+				)
+			}
+
+			ndims := dm.GetArrayDim()
+			if m.ArrayDim != ndims {
+				t.Fatalf(
+					"%s.%s: ndims error. want=%d. got=%d\n",
+					table.Name, m.Name, m.TypeName,
+					ndims,
+				)
+			}
+		}
+	}
 }
